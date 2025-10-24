251add27cb4d265fbd28b09aeb12b12e7f7064a4
srinathmkce <shrinath.suresh@gmail.com>
2025-10-24 23:57:54 +0530

    Adding a wrapper predict_fn for evaluation

--- NLP/mlflow_examples/lc_model.py
+++ NLP/mlflow_examples/lc_model.py
@@ -6,31 +6,30 @@ from mlflow.models import set_model
 
 
 class LangchainModel(PythonModel):
+    rate_limiter = InMemoryRateLimiter(
+        requests_per_second=0.1,  # <-- Super slow! We can only make a request once every 10 seconds!!
+        check_every_n_seconds=0.1,  # Wake up every 100 ms to check whether allowed to make a request,
+        max_bucket_size=10,  # Controls the maximum burst size.
+    )
+    llm = ChatGoogleGenerativeAI(
+        model="gemini-2.5-flash-lite",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        rate_limiter=rate_limiter,
+    )
+
     def __init__(self):
         super().__init__()
-        rate_limiter = InMemoryRateLimiter(
-            requests_per_second=0.1,  # <-- Super slow! We can only make a request once every 10 seconds!!
-            check_every_n_seconds=0.1,  # Wake up every 100 ms to check whether allowed to make a request,
-            max_bucket_size=10,  # Controls the maximum burst size.
-        )
-        print("Initializing LLM with rate limiter...")
-        self.llm = ChatGoogleGenerativeAI(
-            model="gemini-2.5-flash",
-            temperature=0,
-            max_tokens=None,
-            timeout=None,
-            max_retries=2,
-            rate_limiter=rate_limiter,
-        )
-        print("LLM initialized.")
 
-    def predict(self, model_input: dict) -> str:
-        article = model_input["article"]
+    @classmethod
+    def predict(cls, model_input: dict) -> str:
         prompt_template = mlflow.genai.load_prompt(
             "news_classifier", version=1
         ).template
-        prompt = prompt_template.format(article=article)
-        response = self.llm.invoke(prompt)
+        prompt = prompt_template.format(article=model_input)
+        response = cls.llm.invoke(prompt)
         print(response)
         return response.content
 
